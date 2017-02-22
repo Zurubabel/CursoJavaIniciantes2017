7e6bfb56e2974dea80855a9babfc6fc19a2f642a
Aulas <Aulas>
2017-02-22 14:02:42 -0300

    Aula 7 - Variáveis primitivas

--- src/curso/java/CursoJava.java
+++ src/curso/java/CursoJava.java
@@ -3,7 +3,21 @@ package curso.java;
 public class CursoJava {
 
     public static void main(String[] args) {
+        // Variáveis
+        // Tipo nome = valor
         
+        // Variáveis primitivos
+        // Inteiros
+        int inteiro = 10;
+        long longo = 100L;
+        
+        // Reais
+        float flutuante = 10.0f;
+        double dobro =10.0;
+        
+        long resposta = inteiro + longo;
+        
+        System.out.println(resposta);
         
         
         
