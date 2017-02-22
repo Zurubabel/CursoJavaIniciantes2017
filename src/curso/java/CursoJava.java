d7c9e3f95184b775cd8adba38ba10d746391d837
Aulas <Aulas>
2017-02-22 13:48:28 -0300

    Máquina de suco de macho

---
+++ src/curso/java/CursoJava.java
@@ -0,0 +1,13 @@
+package curso.java;
+
+public class CursoJava {
+
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+    }
+    
+}
---
+++ src/maquina/suco/macho/MaquinaSucoMacho.java
@@ -0,0 +1,34 @@
+package maquina.suco.macho;
+
+public class MaquinaSucoMacho {
+    
+    public static void main(String args[]) {
+        /*
+            Máquina de Suco de Macho
+            
+            Tamanho do copo = 500ml
+        
+            Dose de açucar = 5ml
+        
+            Água = 50ml
+        
+            Suco = 10ml
+        
+            Jailson = Gosto
+        
+            * Gerar o gosto do Jailson aleatoriamente
+                - Retornar o gosto dele
+            * Controles do suco
+            * Controlar o volume do copo
+        */
+        
+        int tamanhoCopo = 500;
+        int doseAcucar = 5;
+        int agua = 50;
+        int suco = 10;
+        
+        System.out.println(tamanhoCopo + agua);
+        
+        
+    }
+}
