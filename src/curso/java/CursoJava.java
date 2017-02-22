06bdefb6a4e79410360e16ae55ea20bfbea50251
Aulas <Aulas>
2017-02-22 16:42:14 -0300

    Aula 12 - Um pouco mais sobre IFS

--- src/curso/java/CursoJava.java
+++ src/curso/java/CursoJava.java
@@ -3,7 +3,7 @@ package curso.java;
 public class CursoJava {
 
     public static void main(String[] args) { 
-        int numero = 0;
+        int numero = -50;
         
         int resultado = numero % 2; 
         
@@ -13,14 +13,16 @@ public class CursoJava {
             System.out.println(numero + " é ímpar");
         }
         
-        if (numero == 0 || numero < 100) {
+        if (numero < 0) {
+            System.out.println(numero + " é negativo");
+        } else if (numero >= 0 && numero < 100) {
             if (numero == 0) {
                 System.out.println(numero + " é igual a 0");
             } else {
                 System.out.println(numero + " é menor que 100");
             }
             
-        } else if (numero >= 100 || numero <= 200) {
+        } else if (numero >= 100 && numero <= 200) {
             System.out.println(numero + " é maior ou igual a 100 ou menor ou igual a 200");
         } else {
             System.out.println(numero + " é maior que 200");
