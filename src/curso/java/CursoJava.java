9c7663aeb880525d638fcf30403877e16b8d62ad
Aulas <Aulas>
2017-02-27 18:51:20 -0300

    Aula 22 - Escopo das Variáveis

--- src/curso/java/CursoJava.java
+++ src/curso/java/CursoJava.java
@@ -1,16 +1,23 @@
 package curso.java;
 
 public class CursoJava {
+    
+    static int numero = 20;
+    
+    static void somarDoisNumeros(int num1, int num2) {
+        System.out.println(numero);
+        System.out.println(num1 + num2);
+    } 
 
     public static void main(String[] args) { 
+                
+        System.out.println(numero);
         
         somarDoisNumeros(2, 3);
         
-        somarDoisNumeros(10, -30);
+        //somarDoisNumeros(10, -30);
     }
     
-    static void somarDoisNumeros(int num1, int num2) {
-        System.out.println(num1 + num2);
-    } 
+    
     
 }
