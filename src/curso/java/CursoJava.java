9fee74fcc9479e84b08a32cb4f03b77c0aea6240
Aulas <Aulas>
2017-02-27 19:52:53 -0300

    Aula 25 - Retorno de valores nas funções

--- src/curso/java/CursoJava.java
+++ src/curso/java/CursoJava.java
@@ -8,12 +8,21 @@ public class CursoJava {
         System.out.println(numero);
         System.out.println(num1 + num2);
     } 
+    
+    static int retornarSomaDoisNumeros(int num1, int num2) {
+        num1 *= num1;
+        return num1 + num2;
+    } 
 
     public static void main(String[] args) { 
                 
-        System.out.println(numero);
+        //System.out.println(numero);
         
-        somarDoisNumeros(2, 3);
+        //somarDoisNumeros(2, 3);
+        
+        int num0 = 10;
+        
+        System.out.println(retornarSomaDoisNumeros(num0, 7));
         
         //somarDoisNumeros(10, -30);
     }
