b5185f92ca668dd7f2ba0cee779eb932adc57fb1
Aulas <Aulas>
2017-02-27 21:15:49 -0300

    Aula 30 - Estrutura de repetição - FOR

--- src/curso/java/CursoJava.java
+++ src/curso/java/CursoJava.java
@@ -3,17 +3,14 @@ package curso.java;
 public class CursoJava {
     
     public static void main(String[] args) { 
-                
-        int numero = 101;
-        while(numero <= 100) {
-            System.out.println("Número exibido: " + numero);
-            numero++;
-        }
+            
         
-        do {
-            System.out.println("Número exibido: " + numero);
-            numero++;
-        } while(numero <= 100);
+        for(int i = 0; i <= 100; i++) {
+            
+            System.out.println("Número exibido: " + i);
+            
+            
+        }
     }
     
     
