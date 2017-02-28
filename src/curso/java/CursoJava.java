450253f2bf244080cfd9cc1431a929de21680c31
Aulas <Aulas>
2017-02-27 21:04:29 -0300

    Aula 29 - While e Do-While

--- src/curso/java/CursoJava.java
+++ src/curso/java/CursoJava.java
@@ -2,34 +2,19 @@ package curso.java;
 
 public class CursoJava {
     
-    static int numero = 20;
-    
-    static void somarDoisNumeros(int num1, int num2) {
-        System.out.println(numero);
-        System.out.println(num1 + num2);
-    } 
-    
-    static int retornarSomaDoisNumeros(int num1, int num2) {
-        num1 *= num1;
-        return num1 + num2;
-    } 
-
     public static void main(String[] args) { 
                 
-        System.out.println(retornarNomeIngrediente(3));
+        int numero = 101;
+        while(numero <= 100) {
+            System.out.println("Número exibido: " + numero);
+            numero++;
+        }
+        
+        do {
+            System.out.println("Número exibido: " + numero);
+            numero++;
+        } while(numero <= 100);
     }
     
-    static String retornarNomeIngrediente(int menu) {
-        String retorno = "Ingrediente selecionado: ";
-        switch(menu) {
-            case 0:
-                return retorno + " Açucar";
-            case 1:
-                return retorno + "Água";
-            case 2:
-                return retorno + "Suco";
-        }
-        return retorno;
-    }
     
 }
