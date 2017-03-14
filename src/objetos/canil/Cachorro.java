635fff6f45b6439fb64aa7ed5b857e366b31aa9d
Aulas <Aulas>
2017-03-13 22:59:23 -0300

    Aula 41 - Atributos e métodos das classes

--- src/objetos/canil/Cachorro.java
+++ src/objetos/canil/Cachorro.java
@@ -2,8 +2,13 @@ package objetos.canil;
 
 public class Cachorro {
     
+    String nome;
     double peso;
     double comprimento;
     double forca;
+    
+    void correr() {
+        System.out.println(nome + " corre a " + (comprimento * forca) + "cm/s");
+    }
     
 }
--- src/objetos/canil/Canil.java
+++ src/objetos/canil/Canil.java
@@ -5,12 +5,19 @@ public class Canil {
     public static void main(String args[]) {
         
         Cachorro florencio = new Cachorro();
+        florencio.nome = "Florêncio";
         florencio.comprimento = 50.5;
         florencio.peso = 35.3;
         florencio.forca = 4.5;
         
         Cachorro chiquinho = new Cachorro();
+        chiquinho.nome = "Chiquinho";
+        chiquinho.comprimento = 15.5;
+        chiquinho.peso = 35.3;
+        chiquinho.forca = 6.5;
         
+        florencio.correr();
+        chiquinho.correr();
         
         
     }
